84b903a079107482887a70217609c8c9a3d76327
Baynova Ekaterina <ekaterinabainova@gmail.com>
2020-05-09 13:42:25 +0300

    Added emmiters mask

--- phase-monitoring/phase-monitoring/phase-monitoring/phase-monitoring.cpp
+++ phase-monitoring/phase-monitoring/phase-monitoring/phase-monitoring.cpp
@@ -13,9 +13,11 @@ using namespace cv;
 
 int main()
 {
-    Mat background = imread("./background.jpg", IMREAD_GRAYSCALE);
-    Mat frame = imread("./frame.jpg", IMREAD_GRAYSCALE);
-    Mat foregroundMask, imgDiff, stabilized;
+    Mat background = imread("./background.jpg", IMREAD_COLOR);
+    Mat frame = imread("./frame.jpg", IMREAD_COLOR);
+    Mat backgroundGray = imread("./background.jpg", IMREAD_GRAYSCALE);
+    Mat frameGray = imread("./frame.jpg", IMREAD_GRAYSCALE);
+    Mat objectsMask, imgDiff, stabilized, labelMask, stabilizedGray, backgroundHSV;
     vector<Point2f> backPts, framePts;
     vector<uchar> status;
     vector<float> err;
@@ -30,37 +32,48 @@ int main()
     }
 
     // Stabilize image
-    goodFeaturesToTrack(background, backPts, 200, 0.3, 7);
-    calcOpticalFlowPyrLK(background, frame, backPts, framePts, status, err);
+    goodFeaturesToTrack(backgroundGray, backPts, 200, 0.3, 7);
+    calcOpticalFlowPyrLK(backgroundGray, frameGray, backPts, framePts, status, err);
     Mat transformationMatrix = estimateAffinePartial2D(backPts, framePts);
-    warpAffine(frame, stabilized, transformationMatrix, background.size(),
+    warpAffine(frameGray, stabilizedGray, transformationMatrix, backgroundGray.size(),
                INTER_NEAREST | WARP_INVERSE_MAP);
-    
+    warpAffine(frame, stabilized, transformationMatrix, background.size(),
+        INTER_NEAREST | WARP_INVERSE_MAP);
+
     // Crop images
-    int widthShift = round(background.size().width * 0.1);
-    int heightShift = round(background.size().height * 0.1);
+    int widthShift = round(backgroundGray.size().width * 0.1);
+    int heightShift = round(backgroundGray.size().height * 0.1);
     Rect2f roi = Rect2f(widthShift, heightShift,
-                        background.size().width - widthShift*2,
-                        background.size().height - heightShift*2);
+                        backgroundGray.size().width - widthShift*2,
+                        backgroundGray.size().height - heightShift*2);
     background = background(roi);
+    backgroundGray = backgroundGray(roi);
     stabilized = stabilized(roi);
+    stabilizedGray = stabilizedGray(roi);
+
+    // Detect labels of emmiters
+    cvtColor(background, backgroundHSV, COLOR_RGB2HSV);
+    inRange(backgroundHSV, Scalar(30, 150, 150), Scalar(60, 255, 255), labelMask);
 
     // Make a mask containing objects as white areas
-    absdiff(background, stabilized, imgDiff);
-    threshold(imgDiff, foregroundMask, 150, 255, THRESH_BINARY);
-    erode(foregroundMask, foregroundMask, Mat());
+    absdiff(backgroundGray, stabilizedGray, imgDiff);
+    threshold(imgDiff, objectsMask, 100, 255, THRESH_BINARY);
+    erode(objectsMask, objectsMask, Mat());
 
-    namedWindow("Background", WINDOW_NORMAL);
+    /*namedWindow("Background", WINDOW_NORMAL);
     imshow("Background", background);
 
     namedWindow("Stabilized", WINDOW_NORMAL);
     imshow("Stabilized", stabilized);
 
     namedWindow("imgDiff", WINDOW_NORMAL);
-    imshow("imgDiff", imgDiff);
+    imshow("imgDiff", imgDiff);*/
 
-    namedWindow("foregroundMask", WINDOW_NORMAL);
-    imshow("foregroundMask", foregroundMask);
+    namedWindow("objectsMask", WINDOW_NORMAL);
+    imshow("objectsMask", objectsMask);
+
+    namedWindow("labelMask", WINDOW_NORMAL);
+    imshow("labelMask", labelMask);
 
     waitKey(0);
     return 0;

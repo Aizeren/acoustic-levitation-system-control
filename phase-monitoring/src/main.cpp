e94294a2bbc0bd9cac7dd2467a794ae2243c35ea
Baynova Ekaterina <ekaterinabainova@gmail.com>
2020-05-10 10:51:47 +0300

    Updated build

--- phase-monitoring/src/main.cpp
+++ phase-monitoring/src/main.cpp
@@ -1,17 +1,15 @@
 #include "functions.h"
 
-#include <ctime>
-
 int main()
 {
     // initial position - in phase
     // length of wave and length of emitters label in mm
     float waveLen = 8.575, labelLen = 10;
     uchar phaseNum = 0, maxPhaseNum = 24;
-    Mat background = imread("./background.jpg", IMREAD_COLOR);
-    Mat frame = imread("./frame.jpg", IMREAD_COLOR);
-    Mat backgroundGray = imread("./background.jpg", IMREAD_GRAYSCALE);
-    Mat frameGray = imread("./frame.jpg", IMREAD_GRAYSCALE);
+    Mat background = imread("./Resources/background.jpg", IMREAD_COLOR);
+    Mat frame = imread("./Resources/frame.jpg", IMREAD_COLOR);
+    Mat backgroundGray = imread("./Resources/background.jpg", IMREAD_GRAYSCALE);
+    Mat frameGray = imread("./Resources/frame.jpg", IMREAD_GRAYSCALE);
     Mat objectsMask, imgDiff, stabilized, labelMask, stabilizedGray, backgroundHSV;
     vector<Point2f> backPts, framePts;
     vector<uchar> status;
@@ -27,45 +25,41 @@ int main()
         return -1;
     }
 
-    for (int o = 0; o < 10; o++) {
-        int startTime = clock();
-        // Stabilize image
-        goodFeaturesToTrack(backgroundGray, backPts, 200, 0.3, 7);
-        calcOpticalFlowPyrLK(backgroundGray, frameGray, backPts, framePts, status, err);
-        Mat transformationMatrix = estimateAffinePartial2D(backPts, framePts);
-        warpAffine(frameGray, stabilizedGray, transformationMatrix, backgroundGray.size(),
-            WARP_INVERSE_MAP);
-        warpAffine(frame, stabilized, transformationMatrix, background.size(),
-            WARP_INVERSE_MAP);
+    // Stabilize image
+    goodFeaturesToTrack(backgroundGray, backPts, 200, 0.3, 7);
+    calcOpticalFlowPyrLK(backgroundGray, frameGray, backPts, framePts, status, err);
+    Mat transformationMatrix = estimateAffinePartial2D(backPts, framePts);
+    warpAffine(frameGray, stabilizedGray, transformationMatrix, backgroundGray.size(),
+        WARP_INVERSE_MAP);
+    warpAffine(frame, stabilized, transformationMatrix, background.size(),
+        WARP_INVERSE_MAP);
 
-        // Crop images
-        int widthShift = round(backgroundGray.size().width * 0.05);
-        int heightShift = round(backgroundGray.size().height * 0.05);
-        Rect2f roi = Rect2f(widthShift, heightShift,
-            backgroundGray.size().width - widthShift * 2,
-            backgroundGray.size().height - heightShift * 2);
-        /*background = background(roi);
-        backgroundGray = backgroundGray(roi);
-        stabilized = stabilized(roi);
-        stabilizedGray = stabilizedGray(roi);*/
+    // Crop images
+    int widthShift = round(backgroundGray.size().width * 0.05);
+    int heightShift = round(backgroundGray.size().height * 0.05);
+    Rect2f roi = Rect2f(widthShift, heightShift,
+        backgroundGray.size().width - widthShift * 2,
+        backgroundGray.size().height - heightShift * 2);
+    /*background = background(roi);
+    backgroundGray = backgroundGray(roi);
+    stabilized = stabilized(roi);
+    stabilizedGray = stabilizedGray(roi);*/
 
-        // Detect labels of emitters
-        cvtColor(background, backgroundHSV, COLOR_RGB2HSV);
-        inRange(backgroundHSV, Scalar(30, 170, 160), Scalar(60, 255, 255), labelMask);
+    // Detect labels of emitters
+    cvtColor(background, backgroundHSV, COLOR_RGB2HSV);
+    inRange(backgroundHSV, Scalar(30, 170, 160), Scalar(60, 255, 255), labelMask);
 
-        dilate(labelMask, labelMask, Mat());
-        erode(labelMask, labelMask, Mat());
+    dilate(labelMask, labelMask, Mat());
+    erode(labelMask, labelMask, Mat());
 
-        // Make a mask containing objects as white areas
-        absdiff(backgroundGray, stabilizedGray, imgDiff);
-        threshold(imgDiff, objectsMask, 100, 255, THRESH_BINARY);
-        erode(objectsMask, objectsMask, getStructuringElement(MORPH_RECT, Size(4, 4)));
+    // Make a mask containing objects as white areas
+    absdiff(backgroundGray, stabilizedGray, imgDiff);
+    threshold(imgDiff, objectsMask, 100, 255, THRESH_BINARY);
+    erode(objectsMask, objectsMask, getStructuringElement(MORPH_RECT, Size(4, 4)));
 
-        emittersEdges = findEmittersEdges(labelMask);
+    emittersEdges = findEmittersEdges(labelMask);
 
-        nodesCoordinates = calcNodesCoords(emittersEdges, waveLen, labelLen, phaseNum, maxPhaseNum);
-        cout << clock() - startTime << endl;
-    }
+    nodesCoordinates = calcNodesCoords(emittersEdges, waveLen, labelLen, phaseNum, maxPhaseNum);
 
 // !--------------- ONLY OUTPUTS -----------------!
     for (int i = 0; i < nodesCoordinates.size(); i++) {
